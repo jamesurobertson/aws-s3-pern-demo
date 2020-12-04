8af6f4302fab8c973848234ce220e111d793f38f
James Robertson <jamesurobertson@gmail.com>
2020-12-04 09:36:55 -0600

    modified production build routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,9 +16,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 
   // Serve the static assets in the frontend's build folder
@@ -27,9 +25,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
 
