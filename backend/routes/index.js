cf0b1398a59f923bc2f3f0c6591ab087eafd5fcd
James Robertson <jamesurobertson@gmail.com>
2020-12-04 09:46:10 -0600

    serving static assets from frontends build folder

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,16 +16,20 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/build")));
+  router.use(express.static(path.resolve("../../frontend/build")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
   });
 }
 
